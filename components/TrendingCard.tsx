00ad55336616dff79ff20c54f0c9d67668744266
Reece <peardon.reece@g,ail.com>
2025-06-17 14:11:34 -0700

    finished trending movies carousal.

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Link} from "expo-router";
-import {Image, TouchableOpacity, View} from "react-native";
+import {Image, Text, TouchableOpacity, View} from "react-native";
+import MaskedView from "@react-native-masked-view/masked-view";
+import {images} from "@/constants/images";
 
 const TrendingCard = ({movie: {movie_id, title, poster_url}, index}: TrendingCardProps) => {
     return (
@@ -12,8 +14,18 @@ const TrendingCard = ({movie: {movie_id, title, poster_url}, index}: TrendingCar
                     resizeMode='cover'
                 />
                 <View className='absolute bottom-9 -left-3.5 px-2 py-1 rounded-full'>
-
+                    <MaskedView maskElement={
+                        <Text className='font-bold text-white text-6xl'>{index + 1}</Text>
+                    }>
+                        <Image source={images.rankingGradient}
+                               className='size-14'
+                               resizeMode='cover'
+                        />
+                    </MaskedView>
                 </View>
+                <Text className='text-sm font-bold mt-2 text-light-200' numberOfLines={2}>
+                    {title}
+                </Text>
             </TouchableOpacity>
         </Link>
     )
